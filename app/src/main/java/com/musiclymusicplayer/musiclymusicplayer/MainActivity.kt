96aad7b0ffc058e069bdc546b5e803f41b2ab65e
Devesh Mukherjee <deveshmukherjee76@gmail.com>
2021-11-30 04:42:16 +0530

    Added Splash Screen

--- app/src/main/java/com/musiclymusicplayer/musiclymusicplayer/MainActivity.kt
+++ app/src/main/java/com/musiclymusicplayer/musiclymusicplayer/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_MusiclyMusicPlayer)
         setContentView(R.layout.activity_main)
     }
 }
