e4d9f1e0506d0af57e56cb33608be1ba5699a1ef
Devesh Mukherjee <deveshmukherjee76@gmail.com>
2021-11-30 14:52:42 +0530

    Navigation menu added

--- app/src/main/java/com/musiclymusicplayer/musiclymusicplayer/MainActivity.kt
+++ app/src/main/java/com/musiclymusicplayer/musiclymusicplayer/MainActivity.kt
@@ -2,6 +2,8 @@ package com.musiclymusicplayer.musiclymusicplayer
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import com.musiclymusicplayer.musiclymusicplayer.databinding.ActivityMainBinding
 
@@ -9,13 +11,18 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_MusiclyMusicPlayer)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
+
+        toggle = ActionBarDrawerToggle(this,binding.root,R.string.open,R.string.close)
+        binding.root.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         binding.shuffleBtn.setOnClickListener {
             //Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
@@ -32,5 +39,11 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this@MainActivity, PlaylistActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item))
+            return true
+        return super.onOptionsItemSelected(item)
     }
 }
