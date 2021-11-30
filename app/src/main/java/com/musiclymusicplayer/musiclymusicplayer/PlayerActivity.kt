82cfe3d757d5b4dc6c56b8f3b65336471538aa38
Devesh Mukherjee <deveshmukherjee76@gmail.com>
2021-11-30 05:51:35 +0530

    Added Buttons and A new activity_player.xml

---
+++ app/src/main/java/com/musiclymusicplayer/musiclymusicplayer/PlayerActivity.kt
@@ -0,0 +1,11 @@
+package com.musiclymusicplayer.musiclymusicplayer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player)
+    }
+}
