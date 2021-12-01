5074f8f3ffe05b10160cb496b7a2ec4f97dc76b6
Devesh Mukherjee <deveshmukherjee76@gmail.com>
2021-12-01 16:16:24 +0530

    Permission request added

--- app/src/main/java/com/musiclymusicplayer/musiclymusicplayer/MainActivity.kt
+++ app/src/main/java/com/musiclymusicplayer/musiclymusicplayer/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.musiclymusicplayer.musiclymusicplayer
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.musiclymusicplayer.musiclymusicplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +18,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestRuntimePermission()
         setTheme(R.style.Theme_MusiclyMusicPlayer)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -41,9 +45,29 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun requestRuntimePermission(){
+        if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),13)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == 13){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this,"Permission Granted" , Toast.LENGTH_SHORT).show()
+            else
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),13)
+        }
+    }
+
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (toggle.onOptionsItemSelected(item))
             return true
         return super.onOptionsItemSelected(item)
     }
+
 }
